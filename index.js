798ad11a0fe1f36e25a95baad7dd443b64e3a722
Anny Elly <anny.pantojamartin@gmail.com>
2023-11-26 22:11:08 -0300

    primeira execução do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const express = requise(express)
+const express = require("express")
 
 const app = express()
+
+app.get('/', (requisição, resposta)=>{
+    resposta.send("Ola, mundo! ")
+})
+
+app.listen(3000, ()=>{
+    console.log("servidor rodando na porta 3000!")
+})
