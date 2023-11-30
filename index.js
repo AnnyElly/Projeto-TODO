2e27dc2ba56af60d480eb4736b8ada27669e4ecd
Anny Elly <anny.pantojamartin@gmail.com>
2023-11-30 07:59:58 -0300

    iniciando leitura dos dados do banco de dados

--- index.js
+++ index.js
@@ -35,6 +35,23 @@ app.post('/criar', (requisicao, resposta)=>{
 })
 
 app.get('/', (requisição, resposta)=>{
+    const sql = 'SELECT * from tarefas'
+
+    conexao.query(sql, (erro, dados)=>{
+        if (erro){
+            return console.log(erro)
+        }
+
+        console.tarefas = dados.map((dado) =>{
+            return {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: dado.completa === 0 ? false : true
+            }
+        })
+
+    })
+
     resposta.render('home')
 })
 
